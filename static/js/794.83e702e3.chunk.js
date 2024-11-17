105592c0ac60ff1c99d2f7436378389c252b718d
Robin Kindler <robin.kindler@googlemail.com>
2024-11-17 20:48:11 +0100

    Updates

--- static/js/794.83e702e3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkportfolio_app=self.webpackChunkportfolio_app||[]).push([[794],{7794:(e,p,s)=>{s.r(p),s.d(p,{default:()=>l});s(5043);var r=s(579);const l=e=>{let{message:p}=e;return(0,r.jsx)("div",{children:(0,r.jsx)("p",{children:p})})}}}]);
-//# sourceMappingURL=794.83e702e3.chunk.js.map
