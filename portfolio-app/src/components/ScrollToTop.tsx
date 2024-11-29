e5e10a0688a7153a4024eae196fb948b75fa29a9
Robin Kindler <robin.kindler@googlemail.com>
2024-11-29 21:49:59 +0100

    [fix] simplified scrollToTop as dynamic positioning doesn`t work in all mobile browsers.

--- portfolio-app/src/components/ScrollToTop.tsx
+++ portfolio-app/src/components/ScrollToTop.tsx
@@ -7,41 +7,19 @@ interface ScrollToTopProps {
   scrollToTopBlok: ImageStoryblok;
 }
 
-const bottom = 15; // px
-const right = 15; // px
-
 const ScrollToTop = (props: ScrollToTopProps) => {
   const [showScrollToTop, setShowScrollToTop] = useState(false);
-  const [scrollToTopBottomOffset, setScrollToTopBottomOffset] =
-    useState(bottom);
 
   // ScrollToTop visibility.
   useEffect(() => {
     const handleScroll = () => {
-      const footer = document.querySelector("footer");
-      const footerRect = footer?.getBoundingClientRect();
-
       // Sichtbarkeit des ScrollToTop-Buttons
       if (window.scrollY > 200) {
         setShowScrollToTop(true);
       } else {
         setShowScrollToTop(false);
       }
-
-      // Footer-Überprüfung: Dynamischer Abstand, wenn Footer sichtbar ist
-      if (footerRect) {
-        if (footerRect.top < window.innerHeight && footerRect.bottom > 0) {
-          // Footer ist teilweise im sichtbaren Bereich
-          setScrollToTopBottomOffset(
-            Math.max(window.innerHeight - footerRect.top + bottom, bottom)
-          );
-        } else {
-          // Standardposition, wenn Footer nicht sichtbar ist
-          setScrollToTopBottomOffset(bottom);
-        }
-      }
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -63,8 +41,6 @@ const ScrollToTop = (props: ScrollToTopProps) => {
         borderRadius: props.scrollToTopBlok?.borderRadius
           ? `${props.scrollToTopBlok.borderRadius}em`
           : "0em",
-        bottom: `${scrollToTopBottomOffset}px`,
-        right: right + "px",
       }}
     >
       <ImageBlok blok={props.scrollToTopBlok} onCallback={scrollToTop} />
